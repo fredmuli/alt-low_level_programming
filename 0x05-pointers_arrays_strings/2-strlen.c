087c2f5693a2c8cbabfa7f5475a47241e2995447
fredmuli <fnfredmlie@gmail.com>
2023-10-16 13:06:05 -0700

    Print String length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - a function thqat returns the length of a string.
+ * @s: input a string
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	for (; s[len] != '\0'; len++);
+
+	return (len);
+}
